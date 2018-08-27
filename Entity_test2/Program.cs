ee63e7b8ac142d23449281bd1d293c2e1163ff5a
Evgeniy <savchik.jeka@gmail.com>
2018-08-27 20:42:01 +0300

    Добавьте файлы проекта.

---
+++ Entity_test2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (UserContext db = new UserContext())
+            {
+                var users = db.Users;
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+        }
+    }
+}
---
+++ Entity_test2/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Entity_test2
+{
+    class UserContext : DbContext
+    {
+        public UserContext() : base("UsersDB")
+        { }
+        public DbSet<User> Users {get;set;}
+    }
+}
